e5712362f5a208bb17772fb20ee41d8e4606fb63
Louay Hamada <loauy.hamada@gmail.com>
2022-12-20 04:01:37 +0300

    Show tab panel related to the selected tab

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const tabs = [...document.querySelectorAll(".tab-item")];
+const tabpanels = [...document.querySelectorAll(".tab-panel")];
 
 const setSelectedTab = (tab) => {
   const tabHash = tab.hash;
+  
+  tabpanels.forEach((tp) => {
+    const tabpanelID = tp.id;
+    
+    if (tabpanelID === tabHash.substring(1)) {
+      tp.setAttribute("tabindex", "-1");
+      tp.removeAttribute("hidden");
+      tp.querySelector(".content").focus();
+    } else {
+      tp.removeAttribute("tabindex");
+      tp.setAttribute("hidden", "true");
+    }
+  });
 
   tabs.forEach((t) => {
     const hash = t.hash;
